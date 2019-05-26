e0d3c48a25d69303ae63561f78cf44ae40882ef8
Wesley Fukui <wes.fukui@gmail.com>
2019-05-25 19:44:52 -0600

    changed file

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import logo from '../logo.svg';
-import './App.css';
+import '../App.css';
 
 const Home: React.FC = () => {
   return (
-    <div className="App">
+    <div className="">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
